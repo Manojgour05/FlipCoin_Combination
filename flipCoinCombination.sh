9d028d06b7a9f8f5b2b396170bd8b0a33bb1c591
Manojgour05 <manojgour6811@gmail.com>
2020-10-05 20:53:17 +0530

    Refactor:Fliping a coin for Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -32,14 +32,68 @@ do
 esac
  Singlet[$i]=${comb[$random]}
 done
-echo ${Singlet[@]}
-echo "Head=$hcount"
-echo "Tail=$tcount"
-percent_head=$(($hcount*100/$num))
-percent_tail=$(($tcount*100/$num))
-echo "Percent_Tail=$percent_tail%"
-echo "Percent_Head=$percent_head%"
+	echo ${Singlet[@]}
+	echo "Head=$hcount"
+	echo "Tail=$tcount"
+	percent_head=$(($hcount*100/$num))
+	percent_tail=$(($tcount*100/$num))
+	echo "Percent_Tail=$percent_tail%"
+	echo "Percent_Head=$percent_head%"
 }
+
 echo "Enter a number"
 read num
 get_Singlet_Combination $num
+
+# flipCoin Combination for Doublet
+
+get_FlipCoin_Doublet(){
+
+declare -A Doublet
+
+comb=(HH HT TH TT)
+hh=0
+ht=0
+th=0
+tt=0
+
+for((i=0;i<=num;i++))
+do
+	random=$(($RANDOM%4))
+        case $random in
+		0)
+			((hh++))
+	;;
+		1)
+			((ht++))
+	;;
+		2)
+			((th++))
+	;;
+
+		3)
+			((tt++))
+	;;
+esac
+	Doublet[$i]=${comb[$random]}
+done
+
+	echo ${Doublet[@]}
+	echo "hhCount=$hh"
+	echo "htCount=$ht"
+	echo "thCount=$th"
+	echo "ttCount=$tt"
+	pHH=$(($hh*100/$num))
+	pHT=$(($ht*100/$num))
+	pTH=$(($th*100/$num))
+	pTT=$(($tt*100/$num))
+
+	echo "Percent_HH=$pHH%"
+	echo "Percent_HT=$pHT%"
+	echo "Percent_TH=$pTH%"
+	echo "Percent_TT=$pTT%"
+
+}
+echo "enter a number"
+read num
+get_FlipCoin_Doublet $num
