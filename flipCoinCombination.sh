25d663b060be416fa01f310ad9a67f823cda41fc
Manojgour05 <manojgour6811@gmail.com>
2020-10-05 20:21:06 +0530

    Refactor:FlipCombination for singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -13,4 +13,33 @@ else
         echo "Tail winner"
 fi
 
-
+get_Singlet_Combination(){
+declare -A Singlet
+comb=(H T)
+hcount=0
+tcoount=0
+percent_head=0
+for((i=1;i<=$num;i++))
+do
+	random=$(($RANDOM%2))
+    case $random in 
+	0)
+	  ((hcount++))
+	;;
+	1)
+	   ((tcount++))
+	;;
+esac
+ Singlet[$i]=${comb[$random]}
+done
+echo ${Singlet[@]}
+echo "Head=$hcount"
+echo "Tail=$tcount"
+percent_head=$(($hcount*100/$num))
+percent_tail=$(($tcount*100/$num))
+echo "Percent_Tail=$percent_tail%"
+echo "Percent_Head=$percent_head%"
+}
+echo "Enter a number"
+read num
+get_Singlet_Combination $num
