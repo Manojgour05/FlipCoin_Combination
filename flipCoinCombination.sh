0819c3b005634575d49006a489438b63276ffa6e
Manojgour05 <manojgour6811@gmail.com>
2020-10-05 12:46:27 +0530

    Refactor: FlipCoin Simulator

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,15 @@
 
 echo "Displaying winning percentage of Head and Tail Combination in a singlet,DoubletTriplet"
 
+echo "Flip Coin Simulation"
+flipCoin=$(($RANDOM%2))
+
+
+if [ $flipCoin -eq 0 ]
+then
+        echo "Head winner"
+else
+        echo "Tail winner"
+fi
+
+
