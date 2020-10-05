8a51dd23ac5507e1f802eadda2ee5a484632a8de
Manojgour05 <manojgour6811@gmail.com>
2020-10-05 12:41:58 +0530

    Updated: File updated

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
-echo "Flip Coin Simulation"
-flipCoin=$(($RANDOM%2))
-
-
-if [ $flipCoin -eq 0 ]
-then
-        echo "Head winner"
-else
-        echo "Tail winner"
-fi
+echo "Displaying winning percentage of Head and Tail Combination in a singlet,DoubletTriplet"
 
