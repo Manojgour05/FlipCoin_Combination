3ca6302a47790c50cc366c4dd2d00ee89b8d20c6
Manojgour05 <manojgour6811@gmail.com>
2020-10-05 21:44:50 +0530

    Refactor:Fliping coin for Triplte Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -97,3 +97,87 @@ done
 echo "enter a number"
 read num
 get_FlipCoin_Doublet $num
+
+
+# fliping coin for Triplet Combination
+
+
+get_FlipCoinCoimbination_Triplet(){
+
+declare -A Triplet
+
+comb=(HHH HHT HTH HTT THH THT TTH TTT)
+
+hhh=0
+hht=0
+hth=0
+htt=0
+thh=0
+tht=0
+tth=0
+ttt=0
+
+for((i=0;i<=$num;i++))
+do
+	random=$(($RANDOM%8))
+case $random in 
+	0)
+		((hhh++))
+	;;
+	1)
+		((hht++))
+	;;
+	2)
+		((hth++))
+	;;
+	3)
+		((htt++))
+	;;
+	4)
+		((thh++))
+	;;
+	5)
+		((tht++))
+	;;
+	6)
+		((tth++))
+	;;
+	7)
+		((ttt++))
+	;;
+esac
+
+	Triplet[$i]=${comb[$random]}
+done
+
+ 	echo "${Triplet[@]}"
+	echo "HHH=$hhh"
+	echo "HHT=$hht"
+	echo "HTH=$hth"
+	echo "HTT=$htt"
+	echo "THH=$thh"
+	echo "THT=$tht"
+	echo "TTH=$tth"
+	echo "TTT=$ttt"
+	pHHH=$(($hhh*100/$num))
+	pHHT=$(($hht*100/$num))
+	pHTH=$(($hth*100/$num))
+	pHTT=$(($htt*100/$num))
+	pTHH=$(($thh*100/$num))
+	pTHT=$(($tht*100/$num))
+	pTTH=$(($tth*100/$num))
+	pTTT=$(($ttt*100/$num))
+
+	echo "Percent_HHH=$pHHH%"
+	echo "Percent_HHT=$pHHT%"
+	echo "Percent_HTH=$pHTH%"
+	echo "Percent_HTT=$pHTT%"
+	echo "Percent_THH=$pTHH%"
+	echo "Percent_THT=$pTHT%"
+	echo "Percent_TTH=$pTTH%"
+	echo "Percnet_TTT=$pTTT%"
+}
+
+echo "Enter a number"
+read num
+get_FlipCoinCoimbination_Triplet $num
